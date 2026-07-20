82fee4d944818568cfe81b27ae6ba7fe3c20429f
narishan <narishan@qti.qualcomm.com>
2026-07-20 15:04:20 +0530

    qssi_64go :disable virt service in qssi_64go
    Change-Id: I32461e13210388ec4eef1aa9fed0f84505797201

--- qssi_64go.mk
+++ qssi_64go.mk
@@ -337,10 +337,6 @@ PRODUCT_PACKAGES += initial-package-stopped-states-aosp.xml
 # Enable support for APEX updates
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
-#enable virtualization service, please verify if virtualization needs to be updated
-#for low ram targets
-$(call inherit-product, packages/modules/Virtualization/build/apex/product_packages.mk)
-
 ###################################################################################
 # This is the End of target.mk file.
 # Now, Pickup other split product.mk files:
